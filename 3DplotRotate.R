91420f3e2b8d1385ed6a58794a4b0918af81f2c2
rsvpros <shankarro@vcu.edu>
2021-09-20 09:12:37 -0400

    Update 3DplotRotate.R
    Added option for Gaussian curve, since I think thats cooler and more useful to computer science (RBF, statistical central limit theorem for multiple linear regression model, etc.)

--- 3DplotRotate.R
+++ 3DplotRotate.R
@@ -9,7 +9,7 @@
 ThreeDplotRotate <- function(x, y,z, step = 30, rotation = 360, tstep = 0.25) {
      for (THETA in seq(0,rotation,step)) {
           persp(x, y, z, xlab='X Variable', ylab='Y Variable', zlab='Z Variable',
-                main='3D Plot', col='pink', shade=.4, theta = THETA, phi = 15)
+                main='3D Plot', col='green', shade=.4, theta = THETA, phi = 15)
           Sys.sleep(tstep)
      }
 }
@@ -24,8 +24,29 @@ y <- -10:10
 z_values <- function(x, y) {
      sqrt(x ^ 2 + y ^ 2)
 }
-#create z-values
-z = outer(x, y, z_values)
+
+#define function to create z-values
+#Converted Matlab code from: http://www.taggedwiki.zubiaga.org/new_content/86bb8c19da64c02198b8d8d01635be7c#Meaning_of_parameters_for_the_general_equation
+z_valuesGaussian <- function(x, y,sigma_x = 5,sigma_y = 5) {
+     A = 1
+     x0 = 0
+     y0 = 0
+     
+     sigma_x = 6
+     sigma_y = 3
+     
+     for( theta in seq(0, pi, pi/100)){
+     a = cos(theta)^2/2/sigma_x^2 + sin(theta)^2/2/sigma_y^2
+     b = -sin(2*theta)/4/sigma_x^2 + sin(2*theta)/4/sigma_y^2 
+     c = sin(theta)^2/2/sigma_x^2 + cos(theta)^2/2/sigma_y^2
+     }
+     
+     #return
+     A*exp( - (a*(x-x0)^2 + 2*b*(x-x0)*(y-y0) + c*(y-y0)^2)) 
+}
+
+#create z-values, choose z_values for x2 + y2, or z_valuesGaussian for gaussian
+z = outer(x, y, z_valuesGaussian)
 
 
 
